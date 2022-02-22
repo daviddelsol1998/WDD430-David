a4b28ade9889ac8101e43357fc97b0361b8cd4e3
David Del Sol <Dcastro@gap.com>
2022-02-22 16:26:56 -0700

    created new basics project

--- basics/components-basics/src/app/app.module.ts
+++ basics/components-basics/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServersComponent
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule
