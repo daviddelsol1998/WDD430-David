84bbd8f9cfc3a627a8881f30190b100afb48a6ae
David Del Sol <Dcastro@gap.com>
2022-02-23 17:31:00 -0700

    Display the ContactListComponent

--- cms/src/app/contacts/contact.model.ts
+++ cms/src/app/contacts/contact.model.ts
@@ -1,12 +1,18 @@
 export class Contact {
-    public id: number;
+    public id: string;
     public name: string;
     public email: string;
-    public phone: number;
+    public phone: string;
     public imageUrl: string;
     public group: Array<string>;
 
-    constructor(id: number, name: string, email: string, phone: number, imageUrl: string, group?: Array<string>) {
+    constructor(id: string, name: string, email: string, phone: string, imageUrl: string, group?: Array<string>) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.imageUrl = imageUrl;
+        this.group = group;
 
     }
 }
